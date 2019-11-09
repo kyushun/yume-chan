68c3f52bc6fffc9c5b8bb4989a656ccfd8871a1b
kyushun <27068326+kyushun@users.noreply.github.com>
2019-11-09 10:54:10 +0900

    既存ファイルをフォーマット

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-require('dotenv').config();
-const request = require('request');
-const moment = require('moment');
-const Trello = require('./lib/trello');
+require("dotenv").config();
+const request = require("request");
+const moment = require("moment");
+const Trello = require("./lib/trello");
 
 const DEFAULT_VALUES = {
-  Expired: { prefix: '😇', priority: 100 },
-  Soon: { prefix: '🤯', priority: 10 },
-  InProgress: { prefix: '🤗', priority: 5 },
-  Todo: { prefix: '🤫', priority: 2 },
-  Pending: { prefix: '🤔', priority: 1 },
-  Done: { prefix: '😍', priority: 0 },
+  Expired: { prefix: "😇", priority: 100 },
+  Soon: { prefix: "🤯", priority: 10 },
+  InProgress: { prefix: "🤗", priority: 5 },
+  Todo: { prefix: "🤫", priority: 2 },
+  Pending: { prefix: "🤔", priority: 1 },
+  Done: { prefix: "😍", priority: 0 }
 };
 
-if (!process.env.TRELLO_TODO_LIST_ID || !process.env.TRELLO_INPROGRESS_LIST_ID || !process.env.TRELLO_PENDING_LIST_ID
-  || !process.env.TRELLO_DONE_LIST_ID || !process.env.TRELLO_API_KEY || !process.env.TRELLO_API_TOKEN) {
+if (
+  !process.env.TRELLO_TODO_LIST_ID ||
+  !process.env.TRELLO_INPROGRESS_LIST_ID ||
+  !process.env.TRELLO_PENDING_LIST_ID ||
+  !process.env.TRELLO_DONE_LIST_ID ||
+  !process.env.TRELLO_API_KEY ||
+  !process.env.TRELLO_API_TOKEN
+) {
   return console.error(new Error("環境変数設定が不十分です"));
 }
 
@@ -21,9 +27,9 @@ if (!process.env.TRELLO_TODO_LIST_ID || !process.env.TRELLO_INPROGRESS_LIST_ID |
   const todoTasks = await Trello.getTodoCards();
   const inProgressTasks = await Trello.getInProgresCards();
 
-  allTasks = (() => {
+  const allTasks = (() => {
     const buildObj = (task, defaultValues) => {
-      const remainHours = moment(task.due).diff(moment(), 'hours');
+      const remainHours = moment(task.due).diff(moment(), "hours");
 
       let params;
       if (!task.due || remainHours > 48) {
@@ -34,18 +40,24 @@ if (!process.env.TRELLO_TODO_LIST_ID || !process.env.TRELLO_INPROGRESS_LIST_ID |
         params = DEFAULT_VALUES.Expired;
       }
       const prefix = params.prefix;
-      const suffix = task.due ? moment(task.due).format('MM/DD HH:mm') : '';
+      const suffix = task.due ? moment(task.due).format("MM/DD HH:mm") : "";
       const priority = params.priority;
 
       return {
-        name: prefix + ' ' + task.name + `${suffix ? ` (~${suffix})` : ''}`,
+        name: prefix + " " + task.name + `${suffix ? ` (~${suffix})` : ""}`,
         remainHours: task.due ? remainHours : null,
         priority
-      }
-    }
+      };
+    };
     let allTasks = [];
-    Array.prototype.push.apply(allTasks, todoTasks.map(task => buildObj(task, DEFAULT_VALUES.Todo)));
-    Array.prototype.push.apply(allTasks, inProgressTasks.map(task => buildObj(task, DEFAULT_VALUES.InProgress)));
+    Array.prototype.push.apply(
+      allTasks,
+      todoTasks.map(task => buildObj(task, DEFAULT_VALUES.Todo))
+    );
+    Array.prototype.push.apply(
+      allTasks,
+      inProgressTasks.map(task => buildObj(task, DEFAULT_VALUES.InProgress))
+    );
 
     allTasks.sort((a, b) => {
       if (a.priority > b.priority) return -1;
@@ -59,26 +71,35 @@ if (!process.env.TRELLO_TODO_LIST_ID || !process.env.TRELLO_INPROGRESS_LIST_ID |
     return allTasks;
   })();
 
-  let output = 'タスクを報告するよ〜\n';
+  let output = "タスクを報告するよ〜\n";
 
-  output += allTasks.map(task => {
-    return task.name + '\n';
-  }).join('');
+  output += allTasks
+    .map(task => {
+      return task.name + "\n";
+    })
+    .join("");
 
   console.log(output);
 
-  request({
-    url: process.env.SLACK_WEBHOOK_URL,
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      text: output
-    })
-  }, (error, response, body) => {
-    if (error) {
-      return console.error(error);
-    } else if (response.statusCode !== 400) {
-      return console.error(new Error(response.statusCode + ' ' + response.statusMessage + ': ' + body));
+  request(
+    {
+      url: process.env.SLACK_WEBHOOK_URL,
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        text: output
+      })
+    },
+    (error, response, body) => {
+      if (error) {
+        return console.error(error);
+      } else if (response.statusCode !== 400) {
+        return console.error(
+          new Error(
+            response.statusCode + " " + response.statusMessage + ": " + body
+          )
+        );
+      }
     }
-  });
+  );
 })();
